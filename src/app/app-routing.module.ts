50b50623bcae3b90ebf0586fb2b9e3e2695eaa40
Your Name <your_email@example.com>
2024-05-24 15:27:11 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { Product1Component } from './product1/product1.component';
+import { CartComponent } from './cart/cart.component';
+import { WebsiteComponent } from './website/website.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'product1', component: Product1Component },
+  { path: 'cart', component: CartComponent },
+  { path: 'website', component: WebsiteComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerHomeComponent } from './container-home/container-home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { ProductContainerComponent } from './product-container/product-container.component';
+import { Product1Component } from './product1/product1.component';
+import { CartComponent } from './cart/cart.component';
+import { WebsiteComponent } from './website/website.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ContainerHomeComponent,
+    FooterComponent,
+    ProductComponent,
+    ProductContainerComponent,
+    Product1Component,
+    CartComponent,
+    WebsiteComponent,
   ],
   imports: [
     BrowserModule,
